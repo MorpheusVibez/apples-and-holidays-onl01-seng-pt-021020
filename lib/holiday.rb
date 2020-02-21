e15a7678f26e62a661678e7453b985de6f3400ac
Andre Skeene <halodre24@gmail.com>
2020-02-21 15:21:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash each do |season, date|
+  holiday_hash.each do |season, date|
     puts "#{season.capitalize!}"
   end
 end
