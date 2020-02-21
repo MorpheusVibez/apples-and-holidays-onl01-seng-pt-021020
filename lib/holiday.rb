0287bdfc85d6e58d4b4df90c1f1a8f95a27100bb
Andre Skeene <halodre24@gmail.com>
2020-02-21 15:46:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,6 +85,7 @@ def all_supplies_in_holidays(holiday_hash)
       end
       if date.each do |day, acc|
         supers << "#{acc}"
+        
         date_holiday << "#{day.capitalize}: #{supers}"
           puts "  #{date_holiday}"
       end
