f8c02395b3804d02631ef356d45504072015fa35
Andre Skeene <halodre24@gmail.com>
2020-02-21 15:17:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash[:winter.to_s]
+  holiday_hash[:winter]
 end
 
 def all_holidays_with_bbq(holiday_hash)
